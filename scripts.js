1e485a23cb35fb364153da2cd49fde4debb4cd07
Robert Lee <bobbydwise1@hotmail.com>
2019-08-07 15:49:07 -0700

    Create singleLL object.

--- scripts.js
+++ scripts.js
@@ -10,7 +10,23 @@ The list is very long, so making more than one pass is prohibitively expensive.
 Do this in constant space and in one pass.
 */
 
+class llnode{
+  constructor(value,next=null) {
+    this.value = value;
+    this.next = next;
+  }
 
+  addNext(anotherNode) {
+    this.next = anotherNode;
+    return anotherNode;
+  }
+}
+
+let root = new llnode(0)
+let node1 = new llnode(1)
+let node2 = new llnode(2)
+root.addNext(node1)
+node1.addNext(node2)
 
 $(document).ready(function() {
     $('#output-section-1').text(1);
